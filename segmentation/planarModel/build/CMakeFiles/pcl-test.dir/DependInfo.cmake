86041d2686c888038fd1b8445d601aed90a6bcf8
Charbel199 <charbel-boumaroun@outlook.com>
2020-08-11 12:53:50 +0300

    Moved floor removal to planar model segmentation

--- segmentation/planarModel/build/CMakeFiles/pcl-test.dir/DependInfo.cmake
+++ segmentation/planarModel/build/CMakeFiles/pcl-test.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/charbel/Projects/PCLtest/PCLtutorial/filters/floorremoval/main.cpp" "/home/charbel/Projects/PCLtest/PCLtutorial/filters/floorremoval/build/CMakeFiles/pcl-test.dir/main.cpp.o"
+  "/home/charbel/Projects/PCLtest/PCLtutorial/segmentation/planarModel/main.cpp" "/home/charbel/Projects/PCLtest/PCLtutorial/segmentation/planarModel/build/CMakeFiles/pcl-test.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- segmentation/planarModel/build/cmake_install.cmake
+++ segmentation/planarModel/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/charbel/Projects/PCLtest/PCLtutorial/filters/floorremoval
+# Install script for directory: /home/charbel/Projects/PCLtest/PCLtutorial/segmentation/planarModel
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/charbel/Projects/PCLtest/PCLtutorial/filters/floorremoval/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/charbel/Projects/PCLtest/PCLtutorial/segmentation/planarModel/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
