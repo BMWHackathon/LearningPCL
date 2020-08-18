2ee8f34753b006b69084ce6dd8d186272a863875
Charbel199 <charbel-boumaroun@outlook.com>
2020-08-18 12:34:26 +0300

    Generating small cloud for icp

--- matching/icp/main.cpp
+++ matching/icp/main.cpp
@@ -6,20 +6,58 @@ using namespace std;
 int main()
 {
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZ>);
+  cout << "Choose mode: (1) Generated Cloud, (2) Loaded Cloud: " ;
+  int mode;
+  cin >> mode;
+  cout << "Mode chosen: " << mode << endl;
+
+   pcl::PointCloud<pcl::PointXYZ>::Ptr  cloud_in(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZ>);
+  
+  if (mode == 1)
+  {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in1(new pcl::PointCloud<pcl::PointXYZ>(5,1));
+    for (auto &point : *cloud_in1)
+    {    
+      point.x = 1024 * rand() / (RAND_MAX + 1.0f);
+      point.y = 1024 * rand() / (RAND_MAX + 1.0f);
+      point.z = 1024 * rand() / (RAND_MAX + 1.0f);
+    }
+    *cloud_in = *cloud_in1;
+    cout << "Saved " << cloud_in->points.size() << " data points to input:" << endl;
+
+    for (auto &point : *cloud_in)
+      cout << point << endl;
+
+    *cloud_out = *cloud_in;
+
+    cout << "size:" << cloud_out->points.size() << endl;
+    for (auto &point : *cloud_out)
+      point.x += 0.7f;
+
+    cout << "Transformed " << cloud_in->points.size() << " data points:" << endl;
+
+    for (auto &point : *cloud_out)
+      cout << point << endl;
+  }
+  else if (mode == 2)
+  {
+
     pcl::io::loadPCDFile<pcl::PointXYZ>("../../../resources/table.pcd", *cloud_in);
     pcl::io::loadPCDFile<pcl::PointXYZ>("../../../resources/tableReduced.pcd", *cloud_out);
 
-        pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>
-            icp;
-    icp.setInputSource(cloud_in);
-    icp.setInputTarget(cloud_out);
-    pcl::PointCloud<pcl::PointXYZ> Final;
-    icp.align(Final);
+  }
 
-    cout << "Has converged:" << icp.hasConverged() << " Score: " << icp.getFitnessScore() << endl;
-    cout << icp.getFinalTransformation() << endl;
 
-    return 0;
+  pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>
+      icp;
+  icp.setInputSource(cloud_in);
+  icp.setInputTarget(cloud_out);
+  pcl::PointCloud<pcl::PointXYZ> Final;
+  icp.align(Final);
+
+  cout << "Has converged:" << icp.hasConverged() << " Score: " << icp.getFitnessScore() << endl;
+  cout << icp.getFinalTransformation() << endl;
+
+  return 0;
 }
